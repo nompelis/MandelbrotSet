bec20a0b11bc24a8d36ed3605d6505c935490cce
IN <nompelis@nobelware.com>
2019-06-07 11:22:17 -0500

    Added a structure to store the information of the computation results and the grid for a rectangular region on the complex space. The region is made with specified bounds and resolution. IN 2019/06/07

--- main.c
+++ main.c
@@ -85,14 +85,128 @@ void inComplex_OperatorTester()
 
 
 //
+// a struct to hold the Mandelbrot set computation results
+// for a small region of the complex plane
+//
+struct region_s {
+   int im,jm;            // resolution of the rectangle
+   complex_t pA,pB;      // points on the complex plane to define the rectangle
+   complex_t *points;    // array of points on the grid on the complex plane
+   long *it;             // array to store iteration number per grid point
+};
+
+
+//
+// a function to create a small rectangular region of the complex plane
+// (this construction is certainly a little wasteful in terms of memory when
+// it comes to storing the full set of grid points, when we can be storing
+// two arrays of reals, one for the real and one for the imaginary axis)
+//
+int inMandelbrot_MakeRegion( struct region_s *s, 
+                             complex_t pA, complex_t pB, int im, int jm )
+{
+   size_t isize;
+   int i,j,n;
+
+   if( s == NULL ) return(1);
+   if( im <= 0 || jm <= 0 ) return(2);
+
+
+   isize = (size_t) (im*jm);
+   s->points = (complex_t *) malloc( isize * sizeof(complex_t) );
+   s->it     = (long *)      malloc( isize * sizeof(long) );
+   if( s->points == NULL || s->it == NULL ) {
+      if( s->points != NULL ) free( s->points );
+      if( s->it != NULL ) free( s->it );
+      s->points = NULL;
+      s->it = NULL;
+      return(-1);
+   }
+
+   s->im = im;
+   s->jm = jm;
+   s->pA.re = pA.re;
+   s->pA.im = pA.im;
+   s->pB.re = pB.re;
+   s->pB.im = pB.im;
+
+   n = 0;
+   for(j=0;j<jm;++j) {
+      double y = pA.im + (pB.im - pA.im) * ((double) j)/((double) (jm-1));
+      for(i=0;i<im;++i) {
+         double x = pA.re + (pB.re - pA.re) * ((double) i)/((double) (im-1));
+         s->points[n].re = x;
+         s->points[n].im = y;
+         s->it[n] = 0;
+         ++n;
+      }
+   }
+
+   return(0);
+}
+
+
+//
+// a function to write the region's grid to a file in tecplot format
+// (this can be read by Paraview and Gnuplot)
+//
+int inMandelbrot_DumpRegion( struct region_s *s, char filename[] )
+{
+   int i,j,n;
+   FILE *fp;
+
+   fp = fopen( filename, "w" );
+   if( fp == NULL ) return(1);
+
+   fprintf(fp,"#### Region of the complex plane ### \n");
+   fprintf(fp,"#### Point A (%lf, %lf) \n", s->pA.re, s->pA.im );
+   fprintf(fp,"#### Point B (%lf, %lf) \n", s->pB.re, s->pB.im );
+   fprintf(fp,"#### Resolution %d x %d \n", s->im, s->jm );
+   fprintf(fp,"VARIABLES = x y n \n");
+   fprintf(fp,"ZONE T=\"Grid\",\n");
+   fprintf(fp,"     I=,%d, J=%d, F=POINT \n",s->im,s->jm);
+
+   n = 0;
+   for(j=0;j<s->jm;++j) {
+      for(i=0;i<s->im;++i) {
+         fprintf(fp, "%21.16e %21.16e ", s->points[n].re, s->points[n].im );
+         fprintf(fp, "%ld \n", s->it[n] );
+         ++n;
+      }
+   }
+
+   return(0);
+
+
+   fclose( fp );
+
+   return(0);
+}
+
+
+
+//
 // Driver
 //
 
 int main( int argc, char *argv[] )
 {
-// complex_t a = { 1.0, 2.0 };
+   complex_t a = {-2.0,-2.0 };
+   complex_t b = { 2.0, 2.0 };
+   struct region_s reg;
+   int iret;
 
    inComplex_OperatorTester();
+
+   iret = inMandelbrot_MakeRegion( &reg, a, b, 100, 100 );
+   if( iret != 0 ) {
+      printf("Error creating the region: %d \n",iret);
+   } else {
+      printf("Created a %d x %d grid \n",reg.im, reg.jm );
+   }
+
+   inMandelbrot_DumpRegion( &reg, "grid.dat" );
+
 
    return( EXIT_SUCCESS );
 }
